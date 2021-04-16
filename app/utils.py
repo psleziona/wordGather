2c93ef096affa674bbd098a5f13a0fe8c2eef139
pablo <pawel.sleziona@gmail.com>
2021-04-16 09:18:22 +0200

    cors wip

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,5 @@
 import json
 from flask import Response
-from flask_login import current_user
 from app import db
 from app.models import EnglishWords, Users, WordsHandler
 from app.dictionary_scrap import getTranslate
@@ -15,15 +14,15 @@ def gen_res(msg=None):
 def db_add_word(word, user):
     db_word = EnglishWords.query.filter_by(word=word).first()
     if db_word:
-        if db_word in [x.word for x in current_user.words]:
+        if db_word in [x.word for x in user.words]:
             return gen_res('Word in db'), 200
         else:
-            db.session.add(WordsHandler(word=db_word, user=current_user))
+            db.session.add(WordsHandler(word=db_word, user=user))
             db.session.commit()
             return gen_res(), 201
     else:
         if getTranslate(word):
-            db.session.add(WordsHandler(word=EnglishWords.query.filter_by(word=word).first(), user=current_user))
+            db.session.add(WordsHandler(word=EnglishWords.query.filter_by(word=word).first(), user=user))
             db.session.commit()
             return gen_res(), 201
         else:
